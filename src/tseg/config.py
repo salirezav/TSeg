72800243f6bdce640039e846eda6b604de728298
Alireza Vaezi <s.alireza.v@gmail.com>
2024-12-31 21:57:45 -0500

    add TsegStyles class for button styling in the application

--- src/tseg/config.py
+++ src/tseg/config.py
@@ -3,3 +3,22 @@ import os
 shared_config = {}
 output_dir = os.path.join(os.path.expanduser("~"), "tseg_output")
 shared_config["output_dir"] = output_dir
+
+
+class TsegStyles:
+    BTN_GREEN = """
+    QPushButton{
+    background-color: #198754;
+    border-color: #28a745;
+    }
+    QPushButton::hover
+    {
+    background-color: #218838;
+    border-color: #1e7e34;
+    }
+    QPushButton::pressed
+    {
+    background-color: #1e7e34;
+    border-color: #1c7430;
+    }
+    """
