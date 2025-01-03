68a0186d2209520828201e9097a75385a304fed9
Alireza Vaezi <s.alireza.v@gmail.com>
2025-01-03 12:58:48 -0500

    documentation and comments

--- src/tseg/config.py
+++ src/tseg/config.py
@@ -1,11 +1,19 @@
 import os
 
+# Dictionary to hold shared configuration settings
 shared_config = {}
+
+# Define the output directory path, which is a folder named 'tseg_output' in the user's home directory
 output_dir = os.path.join(os.path.expanduser("~"), "tseg_output")
+
+# Store the output directory path in the shared configuration dictionary
 shared_config["output_dir"] = output_dir
 
-
 class TsegStyles:
+    """
+    A class to hold style definitions for the TSEG application.
+    Currently, it contains styles for a green button.
+    """
     BTN_GREEN = """
     QPushButton{
     background-color: #007bff;
